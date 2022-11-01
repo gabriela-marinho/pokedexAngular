c2d6e33d24b3c6c8355912288af276b61ae921bc
@GabrielaBMarinho <g.barreto.marinho@gmail.com>
2022-11-01 10:00:55 -0300

    primeiro comiit pokedex angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    //dessa forma sei q sempre vamos precisar carregar as paginas desse modulo abaixo
+    loadChildren: () => import('./pages/pages.module').then(p => p.PagesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
